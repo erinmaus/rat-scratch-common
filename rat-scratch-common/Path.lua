fd488280393f1867bd2bd98754280345dd9328d5
Erin Maus <squeak@erinmaus.com>
2026-06-21 14:39:01 -0400

    Add `Path`.

---
+++ rat-scratch-common/Path.lua
@@ -0,0 +1,52 @@
+local Path = {}
+
+--- @param absolutePath string
+--- @param relativePath string
+--- @param rootPath? string
+--- @return string
+function Path.resolve(absolutePath, relativePath, rootPath)
+	if rootPath then
+		absolutePath = absolutePath:gsub("(@)", rootPath)
+	end
+
+	if relativePath:match("^/") then
+		local result = relativePath:gsub("^/", "")
+		return result
+	end
+
+	local resultPathComponents = {}
+	for segment in absolutePath:gmatch("[^/]+") do
+		table.insert(resultPathComponents, segment)
+	end
+
+	if #resultPathComponents > 0 then
+		if love.filesystem.getInfo(absolutePath, "file") then
+			table.remove(resultPathComponents)
+		end
+	end
+
+	local relativeSegments = {}
+	for segment in relativePath:gmatch("[^/]+") do
+		table.insert(relativeSegments, segment)
+	end
+
+	for i, segment in ipairs(relativeSegments) do
+		if i == 1 and segment == "@" then
+			if rootPath then
+				table.insert(resultPathComponents, rootPath)
+			end
+		elseif segment == "." then
+			-- Nothing.
+		elseif segment == ".." then
+			if #resultPathComponents > 0 then
+				table.remove(resultPathComponents)
+			end
+		else
+			table.insert(resultPathComponents, segment)
+		end
+	end
+
+	return table.concat(resultPathComponents, "/")
+end
+
+return Path
--- rat-scratch-common/init.lua
+++ rat-scratch-common/init.lua
@@ -2,5 +2,6 @@ return {
 	Debug = require "rat-scratch-common.Debug",
 	Object = require "rat-scratch-common.Object",
 	FlatTable = require "rat-scratch-common.FlatTable",
+	Path = require "rat-scratch-common.Path",
 	Table = require "rat-scratch-common.Table",
 }
