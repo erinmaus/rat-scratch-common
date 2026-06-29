bbc0d2b3ac8c031301288a9e496cdce005a464b1
Erin Maus <squeak@erinmaus.com>
2026-06-29 09:54:00 -0400

    Fix to SDF sample.

--- samples/math/sdf/init.lua
+++ samples/math/sdf/init.lua
@@ -8,8 +8,7 @@ local id, w, h, polygons = ...
 local imageData = love.image.newImageData(w, h, "rgba8")
 local before = love.timer.getTime()
 imageData:mapPixel(function(x, y)
-	--local distance = SDF.distanceFromPolygons(x, y, polygons)
-	local distance = SDF.distanceFromCircle(x, y, 100, 100, 40)
+	local distance = SDF.distanceFromPolygons(x, y, polygons)
 	local value = math.min(math.abs(distance / 255), 1)
 	if distance < 0 then
 		return 0, value, 0, 1
