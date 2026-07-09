971ed9129590de83824d24bc682c44358e61aaf1
Erin Maus <squeak@erinmaus.com>
2026-07-09 19:59:22 -0400

    Add "Polygon.linecast"

--- rat-scratch-math/Geometry2D/Polygon.lua
+++ rat-scratch-math/Geometry2D/Polygon.lua
@@ -3,6 +3,9 @@ local Point = require("rat-scratch-math.Geometry2D.Point")
 local Debug = require("rat-scratch-common").Debug
 local Common = require("rat-scratch-math.Common")
 local FlatTable = require("rat-scratch-common").FlatTable
+local assert = require("rat-scratch-common").Debug.assert
+local Table = require("rat-scratch-common").Table
+local TablePool = require("rat-scratch-common.TablePool")
 
 local Polygon = {}
 local PolygonImpl = {}
@@ -229,6 +232,43 @@ function Polygon.isCounterClockwise(points, length)
 	return Polygon.shoelace(points, length) > 0
 end
 
+--- @param points number[]
+--- @param length? integer
+--- @return "cw" | "ccw"
+function Polygon.winding(points, length)
+	local sum = Polygon.shoelace(points, length)
+	local sign = Common.zerosign(sum)
+	Debug.assert(sign ~= 0, "polygon is degenerate (shoelace == 0)")
+
+	return sign < 0 and "cw" or "ccw"
+end
+
+--- @param winding "cw" | "ccw"
+--- @param side -1 | 0 | 1
+--- @return "inside" | "outside" | "collinear"
+function Polygon.side(winding, side)
+	if side == 0 then
+		return "collinear"
+	end
+
+	local expected = winding == "ccw" and 1 or -1
+	return side == expected and "outside" or "inside"
+end
+
+--- @param winding "cw" | "ccw"
+--- @param desiredSide "inside" | "outside" | "collinear"
+--- @param cross number
+--- @return boolean, number
+function Polygon.isOnSide(winding, desiredSide, cross)
+	local side = Polygon.side(winding, Common.zerosign(cross))
+
+	if side == desiredSide then
+		return true, math.abs(cross)
+	end
+
+	return false, 0
+end
+
 do
 	local wrappedPolygon = FlatTable.wrap(0, 2)
 
@@ -334,6 +374,135 @@ end
 do
 	local wrappedPolygon = FlatTable.wrap(0, 2)
 
+	local hitPool = TablePool()
+	local cachedPendingHits = {}
+	local cachedHits = {}
+
+	local function _lessHit(a, b)
+		local au = a[2]
+		local bu = b[2]
+
+		if Common.equal(au, bu) then
+			local edgeA = a[1]
+			local edgeB = b[1]
+
+			return edgeA < edgeB
+		end
+
+		return au < bu
+	end
+
+	--- @param ax number
+	--- @param ay number
+	--- @param bx number
+	--- @param by number
+	--- @param points number[]
+	--- @param length? integer
+	--- @return boolean, integer?, number?, number?, integer?, number?, number?
+	function Polygon.linecast(ax, ay, bx, by, points, length)
+		length = length or math.ceil(#points / 2)
+
+		local pool = hitPool
+		pool:reset()
+
+		local pendingHits = cachedPendingHits
+		Table.clear(pendingHits)
+
+		local hits = cachedHits
+		Table.clear(hits)
+
+		local dx, dy
+		local polygon = wrappedPolygon:intrude(points, length)
+		for i = 1, length do
+			local x1, y1 = polygon:get(i)
+			local x2, y2 = polygon:get(i + 1)
+
+			local side1 = Line.sideOfLineSegment(ax, ay, bx, by, x1, y1)
+			local side2 = Line.sideOfLineSegment(ax, ay, bx, by, x2, y2)
+
+			--if (side1 > 0 and side2 < 0) or (side1 < 0 and side2 > 0) then
+			if side1 ~= side2 then
+				local _, rx, ry, u, v =
+					Line.intersection(ax, ay, bx, by, x1, y1, x2, y2)
+
+				if
+					rx
+					and ry
+					and u
+					and v
+					and (
+						Common.greaterThanEqual(v, 0)
+						and Common.lessThanEqual(v, 1)
+					)
+				then
+					local hit = pool:pop()
+					Table.append(hit, i, u, rx, ry)
+					table.insert(pendingHits, hit)
+				end
+			elseif side1 == 0 and side2 == 0 then
+				if not (dx and dy) then
+					dx, dy = Point.directionNormal(ax, ay, bx, by)
+				end
+
+				local sx1, sy1 = x1 - ax, y1 - ay
+				local u1 = Point.dot(sx1, sy1, dx, dy)
+
+				local sx2, sy2 = x2 - ax, y2 - ay
+				local u2 = Point.dot(sx2, sy2, dx, dy)
+
+				local hit1 = pool:pop()
+				local hit2 = pool:pop()
+
+				Table.append(hit1, i, u1, x1, y1)
+				Table.append(hit2, i + 1, u2, x2, y2)
+
+				table.insert(pendingHits, hit1)
+				table.insert(pendingHits, hit2)
+			end
+		end
+
+		if #pendingHits == 0 then
+			return false
+		end
+
+		table.sort(pendingHits, _lessHit)
+		hits[1] = pendingHits[1]
+
+		for i = 2, #pendingHits do
+			local currentHit = hits[#hits]
+			local nextHit = pendingHits[i]
+
+			local currentU = currentHit[2]
+			local nextU = nextHit[2]
+
+			if Common.greaterThan(nextU, currentU) then
+				table.insert(hits, nextHit)
+			end
+		end
+
+		assert(
+			#hits <= 2,
+			"too many hits for convex polygon; got %d, should be 1 or 2",
+			#hits
+		)
+
+		if #hits == 1 then
+			return true, hits[1][1], hits[1][3], hits[1][4]
+		end
+
+		return true,
+			hits[1][1],
+			hits[1][3],
+			hits[1][4],
+			hits[2][1],
+			hits[2][3],
+			hits[2][4]
+	end
+end
+
+do
+	local wrappedPolygon = FlatTable.wrap(0, 2)
+
 	--- @param ax number
 	--- @param ay number
 	--- @param bx number
@@ -342,12 +511,13 @@ do
 	--- @param length? integer
 	--- @param left? number[]
 	--- @param right? number[]
-	--- @return boolean, number[], number[]
+	--- @return boolean, number[], number[], integer?, integer?
 	function Polygon.split(ax, ay, bx, by, points, length, left, right)
 		length = length or math.ceil(#points / 2)
 		left = left or {}
 		right = right or {}
 
+		local index1, index2
 		local polygon = wrappedPolygon:intrude(points, length)
 		for i = 1, length do
 			local x1, y1 = polygon:get(i)
@@ -369,16 +539,29 @@ do
 			if (side1 > 0 and side2 < 0) or (side1 < 0 and side2 > 0) then
 				local _, rx, ry, u, v =
 					Line.intersection(ax, ay, bx, by, x1, y1, x2, y2)
-				if rx and ry and u and v and (v >= 0 and v <= 1) then
+
+				if
+					rx
+					and ry
+					and u
+					and v
+					and (
+						Common.greaterThanEqual(v, 0)
+						and Common.lessThanEqual(v, 1)
+					)
+				then
 					table.insert(left, rx)
 					table.insert(left, ry)
 					table.insert(right, rx)
 					table.insert(right, ry)
+
+					index2 = index1 and i
+					index1 = index1 or i
 				end
 			end
 		end
 
-		return #left >= 6 and #right >= 6, left, right
+		return #left >= 6 and #right >= 6, left, right, index1, index2
 	end
 end
 
