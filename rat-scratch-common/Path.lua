ae382ce2f9aa95b63526581d5089434ba643ed0e
Erin Maus <squeak@erinmaus.com>
2026-07-31 14:04:50 -0400

    Fix doubly nested root paths.

--- rat-scratch-common/Path.lua
+++ rat-scratch-common/Path.lua
@@ -6,8 +6,8 @@ local Path = {}
 --- @return string
 function Path.resolve(absolutePath, relativePath, rootPath)
 	if rootPath then
-		absolutePath = absolutePath:gsub("^(@)", rootPath)
-		relativePath = relativePath:gsub("^(@)", rootPath)
+		absolutePath = absolutePath:gsub("^(@)", "/" .. rootPath)
+		relativePath = relativePath:gsub("^(@)", "/" .. rootPath)
 	end
 
 	if relativePath:match("^/") then
