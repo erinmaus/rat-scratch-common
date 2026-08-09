0fbf63590fcf7663bd7aef5a634c720f2a11b35d
Erin Maus <squeak@erinmaus.com>
2026-08-09 14:10:15 -0400

    Use `ffi.copy`/`ffi.fill`

--- rat-scratch-graphics/Pipeline3D/PipelineBufferByteData.lua
+++ rat-scratch-graphics/Pipeline3D/PipelineBufferByteData.lua
@@ -1,15 +1,9 @@
 local ffi = require("ffi")
-local assert = require("rat-scratch-common").Debug.assert
 local Object = require("rat-scratch-common").Object
 local Table = require("rat-scratch-common").Table
 local BufferFormat = require("rat-scratch-graphics.Graphics3D.BufferFormat")
 local PipelineBufferData =
 	require("rat-scratch-graphics.Pipeline3D.PipelineBufferData")
-
-ffi.cdef([[
-	void *memcpy(void *dest, const void *src, size_t count);
-	void *memset(void *dest, int ch, size_t count);
-]])
 
 --- @class RatScratch.Graphics.Pipeline3D.PipelineBufferByteData : RatScratch.Graphics.Pipeline3D.PipelineBufferData
 --- @overload fun(format: RatScratch.Graphics.Graphics3D.MeshFormatAttribute[], count: integer): RatScratch.Graphics.Pipeline3D.PipelineBufferByteData
@@ -22,7 +16,6 @@ local PipelineBufferByteData = Object(PipelineBufferData)
 function PipelineBufferByteData:new(format, count)
 	PipelineBufferData.new(self, format, count)
 
-	local componentCount = self:getFormat():getComponentCount()
 	self.bufferData =
 		love.data.newByteData(count * self:getFormat():getStride())
 end
@@ -30,11 +23,10 @@ end
 function PipelineBufferByteData:resize(newCount)
 	local oldCount = self:getCount()
 	local format = self:getFormat()
-	local componentCount = format:getComponentCount()
 
 	local oldBufferData = self.bufferData
 	local newBufferData = love.data.newByteData(newCount * format:getStride())
-	ffi.C.memcpy(
+	ffi.copy(
 		newBufferData:getFFIPointer(),
 		oldBufferData:getFFIPointer(),
 		oldCount * format:getStride()
@@ -48,18 +40,8 @@ end
 
 function PipelineBufferByteData:initialize(index, count)
 	local pointer = ffi.cast("uint8_t*", self.bufferData:getFFIPointer())
-	local p = pointer
 	pointer = pointer + (index - 1) * self:getFormat():getStride()
-	assert(
-		tonumber(ffi.cast("uint32_t", pointer - p)) < self.bufferData:getSize(),
-		"tf"
-	)
-	assert(
-		(index - 1) + count * self:getFormat():getStride()
-			< self.bufferData:getSize(),
-		"wtf"
-	)
-	ffi.C.memset(pointer, 0, count * self:getFormat():getStride())
+	ffi.fill(pointer, count * self:getFormat():getStride(), 0)
 end
 
 do
@@ -90,15 +72,15 @@ end
 function PipelineBufferByteData:copyFromData(index, count, data, offset)
 	offset = offset or 0
 
-	local sourcePointer = ffi.cast("uin8_t*", data:getFFIPointer())
+	local sourcePointer = ffi.cast("uint8_t*", data:getFFIPointer())
 	sourcePointer = sourcePointer + offset
 
 	local destinationPointer =
-		ffi.cast("uin8_t*", self.bufferData:getFFIPointer())
+		ffi.cast("uint8_t*", self.bufferData:getFFIPointer())
 	destinationPointer = destinationPointer
 		+ (index - 1) * self:getFormat():getStride()
 
-	ffi.C.memcpy(
+	ffi.copy(
 		destinationPointer,
 		sourcePointer,
 		count * self:getFormat():getStride()
