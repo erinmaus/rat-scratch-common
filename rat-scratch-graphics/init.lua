454ccc0aea46224d1d704c00d3de6ec3330325ea
Erin Maus <squeak@erinmaus.com>
2026-08-17 16:05:07 -0400

    Expose Atlas.

--- rat-scratch-graphics/init.lua
+++ rat-scratch-graphics/init.lua
@@ -1,4 +1,5 @@
 return {
+	Atlas = require("rat-scratch-graphics.Atlas"),
 	Graphics3D = require("rat-scratch-graphics.Graphics3D"),
 	ShaderPreprocessor = require("rat-scratch-graphics.ShaderPreprocessor"),
 }
