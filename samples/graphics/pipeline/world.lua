b8f63b3f306b5be19df8b0aeb0ad716abda1686b
Erin Maus <squeak@erinmaus.com>
2026-09-13 20:07:51 -0400

    Try to draw?

--- samples/graphics/pipeline/world.lua
+++ samples/graphics/pipeline/world.lua
@@ -1,10 +1,13 @@
-local GLTF = require("rat-scratch-gltf")
-local ExtendedScene = require("rat-scratch-pipeline-tools").Model.ExtendedScene
-local PipelineConfig = require("rat-scratch-pipeline").PipelineConfig
 local PipelineRuntime = require("rat-scratch-pipeline").PipelineRuntime
-local World = require("rat-scratch-pipeline").World
 local ResourceLoader = require("rat-scratch-resource").ResourceLoader
 local Scene = require("rat-scratch-pipeline").Scene
+local PipelineRenderer = require("rat-scratch-pipeline").PipelineRenderer
+local AmbientLight = require("rat-scratch-pipeline").AmbientLight
+local DirectionalLight = require("rat-scratch-pipeline").DirectionalLight
+local Vector3 = require("rat-scratch-math").Vector3
+local Quaternion = require("rat-scratch-math").Quaternion
+local ArcballCamera = require("rat-scratch-pipeline").ArcballCamera
+local LightPipeline = require("rat-scratch-pipeline").LightPipeline
 local PipelineSceneResourceType =
 	require("rat-scratch-pipeline").Resources.PipelineSceneResourceType
 local PipelineScenePointer =
@@ -16,15 +19,10 @@ local demo = {}
 function demo.load()
 	ResourceLoader.toggleDebug(true)
 
-	local parser = GLTF.loadFromFilesystem("samples/assets/gltf/shoe.glb")
-	local baseScene = parser:loadScene(1)
-
 	local pipelineRuntime = PipelineRuntime.loadDefault()
 
-	local extendedScene = ExtendedScene(baseScene, pipelineRuntime:getConfig())
-	local extendedModel = extendedScene:getModel(1)
-
-	local world = World(pipelineRuntime)
+	local renderer = PipelineRenderer(pipelineRuntime)
+	local world = renderer:getWorld()
 	local scene = Scene(world)
 	local object = world:newObject()
 	object:move(scene)
@@ -38,14 +36,34 @@ function demo.load()
 
 	world:getPipeline(AnimationPipeline):loadDefaultShaders()
 
-	demo.world = world
+	local ambientLight = scene:newLight(AmbientLight)
+	ambientLight:setAmbience(0.5)
+
+	local directionalLight = scene:newLight(DirectionalLight)
+	directionalLight:setDirection(Vector3(1, 4, 1))
+
+	local camera = ArcballCamera()
+	camera:setDistance(10)
+	scene:setCamera(camera)
+	scene:getPipeline(LightPipeline):loadDefaultShaders()
+
+	demo.renderer = renderer
 	demo.scene = scene
+	demo.camera = camera
 end
 
 function demo.update()
+	demo.camera:setRotation(
+		Quaternion.fromAxisAngle(Vector3.UNIT_Y, love.timer.getTime() / math.pi)
+	)
+
 	ResourceLoader.update()
-	demo.world:flush()
+	demo.renderer:update()
 	demo.scene:flush()
+end
+
+function demo.draw()
+	demo.renderer:draw(demo.scene)
 end
 
 return demo
